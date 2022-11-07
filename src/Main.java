23f92d3a942e6be86e50bb94fabdc6167cd22054
a123 <shchevelev.roman@yandex.ru>
2022-11-07 14:57:11 +0300

    Домашнее задание № 2 (Корректировка красоты кода)

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,40 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        var dog = 8.0; // начало д/з №1
-        dog = dog + 4.0;
-        dog = dog - 3.5;// значение переменной должна быть 3.5
-        System.out.println(dog);
+
+        //Задача №1
+        var dog = 8.0;
         var cat = 3.6;
-        cat = cat + 4;
-        cat = cat - 1.6;
-        System.out.println(cat);
         var paper = 763789;
-        paper = paper + 4;
-        paper = paper - 7639;
+        System.out.println(dog);
+        System.out.println(cat);
         System.out.println(paper);
 
-        var friend = 19;// начало д/з №2
+        //Задача №2
+        dog = dog + 4.0;
+        cat = cat + 4;
+        paper = paper + 4;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        //Задача №3
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        //Задача №4
+        var friend = 19;
         System.out.println(friend);
         friend = friend + 2;
         System.out.println(friend);
         friend = friend / 7;
         System.out.println(friend);
+
+        //Задача №5
         var frog = 3.5;
         frog = frog * 10;
         System.out.println(frog);
@@ -28,20 +43,26 @@ public class Main {
         frog = frog + 4;
         System.out.println(frog);
 
+        //Задача №6
         var weightBoxer1 = 78.2;
         var weightBoxer2 = 82.7;
         var weightTotal = weightBoxer1 + weightBoxer2;
         System.out.println("Общий вес бойцов " + weightTotal + " кг!");
         var weightOdds = weightBoxer2 - weightBoxer1;
         System.out.println("Разница в весе между бойцами " + weightOdds + " кг!");
+
+        //Задача №7
+        System.out.println("Разница в весе между бойцами " + weightOdds + " кг!");
         var overLoadWeight = weightBoxer2 % weightBoxer1;
         System.out.println("Боец номер 2, тяжелее на " + overLoadWeight + " кг!");
 
+        //Задача №8.1
         var totalTimeWork = 640;
         var timeworker = 8;
         var totalWorker = totalTimeWork / timeworker;
         System.out.println("Всего работников компании - " + totalWorker + " человек");
 
+        //Задача №8.2
         var totalWorkerPlus = totalWorker + 94;
         var timeWorkerPlus = totalTimeWork / totalWorkerPlus;
         System.out.println("Если в компании работает " + totalWorkerPlus + " человек, то всего " + timeWorkerPlus + " часов работы может быть поделеномежду сотрудниками");
